4496614d25cde678c21b5106a0bdd9ec3f8b3dfa
EM726 <manbo726015@gmail.com>
2022-10-08 21:30:40 +0900

    makepost-crud1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_04_101339) do
+ActiveRecord::Schema.define(version: 2022_10_08_122309) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
