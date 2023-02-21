424d5ccb6d974fa05d1bf748bca03623224c525d
EM726 <manbo726015@gmail.com>
2023-02-21 19:20:07 +0900

    finish-3

--- db/migrate/20221221153332_posts_image.rb
+++
@@ -1,5 +0,0 @@
-class PostsImage < ActiveRecord::Migration[5.2]
-    def up
-      add_column :posts, :post_image,:text
-    end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_21_153332) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string "content", limit: 1000, default: "", null: false
-    t.integer "post_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "parent_id"
-    t.index ["parent_id"], name: "index_comments_on_parent_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.text "post_image"
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2022_09_28_092958) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,7 +20,6 @@ ActiveRecord::Schema.define(version: 2022_12_21_153332) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
